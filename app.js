d748f6dceba769865176e6dd5e349e73d77e2b8b
Jose Piras <giuseppe3piras@gmail.com>
2022-09-27 20:20:26 +0200

    Completed but some bugs might occured

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+
+app.get("/", (request, response, next) => {
+    console.log(__dirname)
+
+    // response.send("hello world");
+
+    response.sendFile(__dirname + '/views/index.html');
+
+});
+
+
+
+
+app.get("/about.html", (request, response, next) => {
+    response.sendFile(__dirname + './about.html');
+
+});
+app.get("/gallery", (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery.html');
+
+});
+app.get("/work", (request, response, next) => {
+    response.sendFile(__dirname + '/views/work.html');
+
+});
+
+
+
+app.listen(3010, () => console.log('My first app listening on port 3000! '));
